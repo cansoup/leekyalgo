1286ff39aab3b238398d28a8fca270191bfea933
cansoup <59472641+cansoup@users.noreply.github.com>
2026-07-08 23:30:43 +1000

    Update 0003-longest-substring-without-repeating-characters.ts

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,27 +1,26 @@
 function lengthOfLongestSubstring(s: string): number {
-    let index = 0 
+    let index = 0
     let maxlen = 0
     let currlen = 0
     let currStr = ""
 
-    while(index < s.length) {
-        // 아직 없으면
-        // currStr에 현재 문자 추가
-        // currlen 갱신
+    while (index < s.length) {
+        // if not appeared yet
+        // - add current letter to currStr
+        // - update currlen
         if (!currStr.includes(s[index])) {
             currStr = `${currStr}${s[index]}`
             currlen++;
         } else {
-            // 이미 있는 문자
-            // 가장 가까운 문자의 index를 찾음
+            // if already appeared
+            // look for the closest index of the same letter
             const indexOfPreviousLetter = s.slice(0, index).lastIndexOf(s[index])
-            // currStr 초기화
-            // 조건에 따라 maxlen 갱신
-            console.log(indexOfPreviousLetter)
+            // update currStr
             currStr = `${s.slice(indexOfPreviousLetter, index)}`
             currlen = currStr.length
         }
 
+        // update maxlen
         maxlen = maxlen < currlen ? currlen : maxlen;
         index++;
     }
@@ -29,4 +28,4 @@ function lengthOfLongestSubstring(s: string): number {
     return maxlen
 };
 
-// Failure 1: anviaj와 같이 중복되는 글자가 바로 전에 등장하는 게 아니라 한참 전에 등장하는 경우에 대한 고려를 안함
+// Failure 1: Haven't consider the case lie "anviaj". Only consider the case that duplicated characters appear right before current index, such as "pwwkew"
