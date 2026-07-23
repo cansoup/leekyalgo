b916567f7a4ca4fdff098a7742784bab81f9d3d6
cansoup <59472641+cansoup@users.noreply.github.com>
2026-07-23 19:37:38 +1000

    Time: 0 ms (100%), Space: 58.9 MB (28.89%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.ts
@@ -0,0 +1,33 @@
+function myAtoi(s: string): number {
+    // 1. ignore white space
+    const ignoreWhiteSpace = s.trim();
+    
+    let positiveFlag = 1;
+    let result = "";
+    for(let i=0; i<ignoreWhiteSpace.length; i++) {
+
+        if(i === 0) {
+            switch (ignoreWhiteSpace[0]){
+                case "-":
+                    positiveFlag = -1;
+                    continue;
+                case "+":
+                    positiveFlag = 1;
+                    continue;
+            }
+        }
+
+        if(!isInteger(ignoreWhiteSpace[i])) break;
+        result += ignoreWhiteSpace[i];
+    }
+
+    if(!isInteger(result)) return 0
+    if(parseInt(result) * positiveFlag < (-2)**31) return (-2)**31
+    if(parseInt(result) * positiveFlag > 2**31 - 1) return 2**31 - 1
+
+    return parseInt(result) * positiveFlag;
+};
+
+function isInteger(s: string): boolean {
+    return !isNaN(parseInt(s)) && typeof parseInt(s) === 'number'
+}
