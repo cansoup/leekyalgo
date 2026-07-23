0785b58521d51789b7799af5ef75d871e0a2d5e9
cansoup <59472641+cansoup@users.noreply.github.com>
2026-07-23 19:43:12 +1000

    Time: 1 ms (91.03%), Space: 57.7 MB (88.26%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.ts
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.ts
@@ -1,33 +1,45 @@
+// funtion using regular expression
 function myAtoi(s: string): number {
-    // 1. ignore white space
-    const ignoreWhiteSpace = s.trim();
-    
-    let positiveFlag = 1;
-    let result = "";
-    for(let i=0; i<ignoreWhiteSpace.length; i++) {
+    const trimmed = s.trim();
+    const match = trimmed.match(/^[+-]?\d+/);
 
-        if(i === 0) {
-            switch (ignoreWhiteSpace[0]){
-                case "-":
-                    positiveFlag = -1;
-                    continue;
-                case "+":
-                    positiveFlag = 1;
-                    continue;
-            }
-        }
+    if (!match) return 0;
 
-        if(!isInteger(ignoreWhiteSpace[i])) break;
-        result += ignoreWhiteSpace[i];
-    }
+    const num = parseInt(match[0], 10);
 
-    if(!isInteger(result)) return 0
-    if(parseInt(result) * positiveFlag < (-2)**31) return (-2)**31
-    if(parseInt(result) * positiveFlag > 2**31 - 1) return 2**31 - 1
-
-    return parseInt(result) * positiveFlag;
-};
-
-function isInteger(s: string): boolean {
-    return !isNaN(parseInt(s)) && typeof parseInt(s) === 'number'
+    return Math.max(Math.min(num, 2 ** 31 - 1), -(2 ** 31));
 }
+
+// function myAtoi(s: string): number {
+//     // 1. ignore white space
+//     const ignoreWhiteSpace = s.trim();
+    
+//     let positiveFlag = 1;
+//     let result = "";
+//     for(let i=0; i<ignoreWhiteSpace.length; i++) {
+
+//         if(i === 0) {
+//             switch (ignoreWhiteSpace[0]){
+//                 case "-":
+//                     positiveFlag = -1;
+//                     continue;
+//                 case "+":
+//                     positiveFlag = 1;
+//                     continue;
+//             }
+//         }
+
+//         if(!isInteger(ignoreWhiteSpace[i])) break;
+//         result += ignoreWhiteSpace[i];
+//     }
+
+//     if(!isInteger(result)) return 0
+//     if(parseInt(result) * positiveFlag < (-2)**31) return (-2)**31
+//     if(parseInt(result) * positiveFlag > 2**31 - 1) return 2**31 - 1
+
+//     return parseInt(result) * positiveFlag;
+// };
+
+// function isInteger(s: string): boolean {
+//     return !isNaN(parseInt(s)) && typeof parseInt(s) === 'number'
+// }
